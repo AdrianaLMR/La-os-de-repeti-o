b3d466380c78134b51bd343a45fdc19810a8df7a
Adriana LMR <trevenlynk038@gmail.com>
2022-05-24 18:53:51 -0300

    Efetuando o primeiro commit de laços de repetição

---
+++ loopFor.dart
@@ -0,0 +1,13 @@
+main(){
+
+ int x;
+ int sum;
+ sum = 0;
+
+ for (x = 1; x <= 10; x++){
+ sum += x;
+  print('$sum');
+}
+  print('sum = $sum');
+
+ }
---
+++ loopWhile.dart
@@ -0,0 +1,35 @@
+main (){
+
+ bool condicao = true;
+ int x = 0;
+
+ while(condicao){
+   print('Rodou $x');
+   if(x > 9){
+     condicao = false;
+   }
+    x++;
+ }
+
+ }
+
+ //  main (){
+
+ // var Lista = [];
+ 
+ //bool condicao = true;
+ 
+// while(condicao){
+// print('Digite a lista de compras');
+ //String text = stdin.readLineSync().toString();
+ //if (text == 'sair'){
+ //print('Lista finalizada');
+// condicao = false;
+// } else {
+// Lista.add(text); 
+// }
+// print(Lista);
+// print('\n');
+//}
+//}
+ 
